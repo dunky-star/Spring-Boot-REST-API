3fb35afb8c859c564750ec2af70a869a175ec394
dunky-star <dukygeoffrey39@gmail.com>
2022-10-28 13:14:40 +0300

    Spring REST - JSON data buinding with Jackson

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Spring-REST-API
+Business problem:- Build a client app that provides the weather report for a city. Need to get weather data from an external service.
---
+++ spring-rest-api-dunky/src/main/java/com/dunky/api/SpringRestApiDunkyApplication.java
@@ -0,0 +1,13 @@
+package com.dunky.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiDunkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiDunkyApplication.class, args);
+	}
+
+}
