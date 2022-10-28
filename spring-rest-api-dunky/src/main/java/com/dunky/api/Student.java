e0af867383c3fbaedd0a09d7c8e0908c20c6ddd8
dunky-star <dukygeoffrey39@gmail.com>
2022-10-28 13:24:01 +0300

    Add Sudent POJO and JSON data files

---
+++ spring-rest-api-dunky/src/main/java/com/dunky/api/Student.java
@@ -0,0 +1,13 @@
+package com.dunky.api;
+
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+
+    public Student(){
+
+    }
+}
