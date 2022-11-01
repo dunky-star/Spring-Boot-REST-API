af9bbf3b4a5f7d95f94b6864e149d557627a9517
dunky-star <dukygeoffrey39@gmail.com>
2022-11-01 13:03:34 +0300

    Spring Boot REST API and Spring MVC app.

--- spring-rest-api-dunky/src/main/java/com/dunky/api/SpringRestApiDunkyApplication.java
+++ spring-rest-api-dunky/src/main/java/com/dunky/api/SpringRestApiDunkyApplication.java
@@ -1,19 +1,15 @@
 package com.dunky.api;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestApiDunkyApplication implements CommandLineRunner {
+public class SpringRestApiDunkyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiDunkyApplication.class, args);
 
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// Code here
-	}
+
 }
